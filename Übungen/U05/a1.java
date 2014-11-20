23d6f4400891b2f0780b65325a4244dc35437179
MrLoh <flojoto@hotmail.com>
2014-11-20 19:39:08 +0100

    finished U5

--- Übungen/U05/a1.java
+++ Übungen/U05/a1.java
@@ -1,15 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 public class a1 {
+	static Random rnd = new Random();
+	static double calls = 0;
 
 	static int binSearch(int[] arr, int key) {
+		calls = 0;
 		return binSearch(arr, key, 0, arr.length-1 );
 	}
 	static int binSearch(int[] arr, int key, int l, int r) {
+		calls ++;
 		if ( l>r )
 			throw new RuntimeException("key not in array");
 		int m = l+(r-l)/2;
-		System.out.println("l: "+l+"\tr: "+r+"\tm: "+m);
+		// System.out.println("l: "+l+"\tr: "+r+"\tm: "+m);
 		if ( key==arr[m] )
 			return arr[m];
 		else if ( key<arr[m] )
@@ -20,13 +25,15 @@ public class a1 {
 
 
 	static int intplSearch(int[] arr, int key) {
+		calls = 0;
 		return intplSearch(arr, key, 0, arr.length-1 );
 	}
 	static int intplSearch(int[] arr, int key, int l, int r) {
+		calls ++;
 		if ( l>r )
 			throw new RuntimeException("key not in array");
-		int m = l+(r-l)*(key-arr[l])/(arr[r]-arr[l]);
-		System.out.println("l: "+l+"\tr: "+r+"\tm: "+m);
+		int m = l+(int)(((double)key-arr[l])/(arr[r]-arr[l])*(r-l));
+		// System.out.println("l: "+l+"\tr: "+r+"\tm: "+m);
 		if ( key==arr[m] )
 			return arr[m];
 		else if ( key<arr[m] )
@@ -36,24 +43,87 @@ public class a1 {
 	}
 
 
-	static int[] newArray(int len){
+	static int[] newArray(int len, int range){
 		int[] arr = new int[len];
 		for ( int i=0; i<len ; i++ )
-			arr[i] = i;
+			arr[i] = rnd.nextInt(range);
+		Arrays.sort(arr);
 		return arr;
 	}
+	static double median(double[] arr) {
+		double out = 0;
+		for ( double i : arr ) {
+			out += i;
+		}
+		return out/arr.length;
+	}
+	static double stdev(double[] arr) {
+		double out = 0;
+		double m = median(arr);
+		for ( double i : arr ) {
+			out += Math.pow(i-m,2);
+		}
+		return Math.sqrt(out/arr.length);
+	}
+
+
 	public static void main(String[] args) {
-		int[] arr = newArray(50);
 
-		System.out.println(Arrays.toString(arr));
-		System.out.println(binSearch(arr, 24));
-		System.out.println(binSearch(arr, 0));
-		System.out.println(binSearch(arr, 36));
-		System.out.println();
+		boolean printa = false;
+		if ( printa ) {
+			try {
+				PrintWriter writer = new PrintWriter("a1.csv", "UTF-8");
+				int range;
+				int found;
+				int iterations = 100;
+				System.out.println("len, median BS, stdev BS, median IS, stdev IS");
+				writer.println("len, median BS, stdev BS, median IS, stdev IS");
+				for ( int len=100; len<100000; len+=100 ) {
+					range = len;
+					// System.out.println("len: "+len+" range: "+range);
+					System.out.print(len+", ");
+					int i = 0;
+					double[] callsBS = new double[iterations];
+					double[] callsIS = new double[iterations];
+					while ( i<iterations ) {
+						int[] arr = newArray(len, range);
+						// System.out.println(Arrays.toString(arr));
+						int key = rnd.nextInt(range);
+						try {
+							found = binSearch(arr, key);
+							callsBS[i] = calls;
+							// System.out.println("found "+found+" in "+calls+" calls");
+							found = intplSearch(arr, key);
+							callsIS[i] = calls;
+							// System.out.println("found "+found+" in "+calls+" calls");
+							i++;
+						} catch( RuntimeException e ){};
+					}
+					System.out.print(median(callsBS)+", "+stdev(callsBS)+", ");
+					System.out.println(median(callsIS)+", "+stdev(callsIS));
+					writer.println(len+","+median(callsBS)+","+stdev(callsBS)+","+median(callsIS)+","+stdev(callsIS));
+				}
+				// System.out.println("\n\n");
+				writer.close();
+			} catch( Exception e ) {
+				System.out.println("File Error");
+			}
+		}
 
-		System.out.println(intplSearch(arr, 24));
-		System.out.println(intplSearch(arr, 2));
-		System.out.println(intplSearch(arr, 40));
-		System.out.println();
+
+		// int[] arr = newArray(20, 20);
+		// System.out.println(Arrays.toString(arr));
+		// intplSearch(arr, 10);
+		// System.out.println(calls);
+		// intplSearch(arr, 5);
+		// System.out.println(calls);
+
+
+		int[] badArr = {1,1,1,1,1,1,1,1,2,99};
+		System.out.println("Take the array: "+Arrays.toString(badArr));
+		System.out.println("then: (key-arr[l])/(arr[r]-arr[l]) = (2-1)/(3-1) = 0");
+		System.out.println("=> m = l");
+		intplSearch(badArr, 2);
+		System.out.println("therefore we have n-1 = "+calls+" calls");
 	}
 }
